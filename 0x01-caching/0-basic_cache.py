9aa111f5d91046e0eb557a09f7461abd9eb2bac5
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-25 20:30:07 +0300

    improve the checking logic

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -17,11 +17,11 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Assigns a new key value pair to the cached_data"""
-        if (key is not None or item is not None):
+        if (key is not None and item is not None):
             self.cache_data[key] = item
 
     def get(self, key):
         """Returns the value in self.cache_data linked to key"""
         if (key is None or key not in self.cache_data.keys()):
             return None
-        return self.cache_data.get(key)
+        return self.cache_data[key]
