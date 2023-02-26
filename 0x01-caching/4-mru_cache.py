c1035d91e5cbe00fd86d02ad433dab1e508dec1b
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-26 21:29:29 +0300

    Implementing the algorithm in a new way

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -10,41 +10,35 @@ from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRU caching """
+    """MRU(Most Recently Used) Cache"""
+    lst = []
 
     def __init__(self):
-        """ Constructor """
+        """Initialization"""
         super().__init__()
-        self.queue = []
 
     def put(self, key, item):
-        """ Puts item in cache """
-        if key is None or item is None:
-            return
+        """Stores a new item to the cache"""
+        if key is not None and item is not None:
+            if key in MRUCache.lst:
+                MRUCache.lst.remove(key)
+                MRUCache.lst.append(key)
+                return
+            self.cache_data[key] = item
+            MRUCache.lst.append(key)
 
-        self.cache_data[key] = item
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            if self.queue:
-                last = self.queue.pop()
-                del self.cache_data[last]
-                print("DISCARD: {}".format(last))
-
-        if key not in self.queue:
-            self.queue.append(key)
-        else:
-            self.mv_last_list(key)
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+            key_to_be_removed = MRUCache.lst[-2]
+            if (key_to_be_removed in self.cache_data.keys()):
+                self.cache_data.pop(key_to_be_removed)
+                print('DISCARD: {}'.format(MRUCache.lst.pop(-2)))
 
     def get(self, key):
-        """ Gets item from cache """
-        item = self.cache_data.get(key, None)
-        if item is not None:
-            self.mv_last_list(key)
-        return item
+        """Returns an item based on the key"""
+        if (key is None or key not in self.cache_data.keys()):
+            return None
+        if key in MRUCache.lst:
+            MRUCache.lst.remove(key)
+            MRUCache.lst.append(key)
 
-    def mv_last_list(self, item):
-        """ Moves element to last idx of list """
-        length = len(self.queue)
-        if self.queue[length - 1] != item:
-            self.queue.remove(item)
-            self.queue.append(item)
+        return self.cache_data[key]
