ef779048a812b48e4a54a07c0048f54f8d9303cb
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-28 22:00:48 +0300

    fix

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -5,12 +5,21 @@ Desc: A simple flask application with babel extension
 Author: Gizachew Bayness
 Date Created: Feb 28, 2023
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config(object):
-    """Config class for Babel configuration"""
+    """
+    a class to configure babel
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -23,7 +32,9 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """Get the local language"""
+    """
+    get_locale - function to get the local selector
+    """
     lcl = request.args.get('locale', None)
     if lcl and lcl in app.config['LANGUAGES']:
         return lcl
@@ -31,9 +42,32 @@ def get_locale():
 
 
 @app.route('/', strict_slashes=False)
-def home():
-    """Home page"""
+def Welcome():
+    """
+    Welcome - a route to a 5-index html
+    """
     return render_template('5-index.html')
+
+
+def get_user():
+    """
+    get_user - function that returns a given user
+    Arguments:
+        Nothing
+    Returns:
+        the user if it is found None other wise
+    """
+    user_id = request.args.get('login_as', None)
+    if user_id is None:
+        return None
+    return users.get(int(user_id))
+
+
+@app.before_request
+def before_request():
+    """A function which will be executed before request has been made"""
+    usr = get_user()
+    g.user = usr
 
 
 if __name__ == "__main__":
