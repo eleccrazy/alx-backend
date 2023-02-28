aa1893075f8773f1f5d64a15b9ecaf933d2a930d
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-28 15:39:59 +0300

    Task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+File: 2-app.py
+Desc: A simple flask application with babel extension
+Author: Gizachew Bayness
+Date Created: Feb 28, 2023
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class for configuring languages and timezons"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def home():
+    """Handles the home route of the flask application"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
